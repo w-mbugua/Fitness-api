9e9e192fe9d23b5093fefe328b6b905a5e744e1a
JoyMbugua <wacumbugua@gmail.com>
2021-05-05 13:37:43 +0300

    fixed the exercise validator method

--- app/auth/v1/model/exercise.py
+++ app/auth/v1/model/exercise.py
@@ -4,29 +4,31 @@ class ExerciseModel:
 
     exercises = []
 
-    def __init__(self, name, period):
+    def __init__(self, name,period, description):
         self.exercise_id = len(ExerciseModel.exercises) + 1
         self.name = name
         self.period = period
+        self.description = description
 
     def save(self):
         data = dict(
-           exercise_id = self.exercise_id,
+            exercise_id = self.exercise_id,
             name = self.name,
-            period = self.period
+            period = self.period,
+            description = self.description
         )
         ExerciseModel.exercises.append(data)
         return ExerciseModel.exercises
 
     def get_exercise_by_id(self, id):
         for exercise in ExerciseModel.exercises:
-            if exercise['exercise_id'] == self.id:
+            if exercise['exercise_id'] == id:
                 return exercise
 
-    # for a keywords search/search endpoint?
-    def get_exercise_by_id(self, name):
+    # for a keywords search/search endpoint? # return a boolean
+    def get_exercise_by_name(self, name):
         for exercise in ExerciseModel.exercises:
-            if exercise['name'] == self.name:
+            if exercise['name'] == name:
                 return exercise
 
     def get_all_exercises(self):
