66fb315aebbcffe70a3a5a8b839b46a47660dd29
JoyMbugua <wacumbugua@gmail.com>
2021-05-05 19:51:19 +0300

    added an endpoint for accessing one routine by id

--- app/auth/v1/__init__.py
+++ app/auth/v1/__init__.py
@@ -2,15 +2,16 @@ from flask import Blueprint
 from flask_restful import Api
 from .views.user_views import Users, User
 from .views.exercise_views import Exercise, SingleExercise
-from .views.workout_routine_views import WorkoutRoutine
+from .views.workout_routine_views import WorkoutRoutine, SingleRoutine
 version1 = Blueprint('auth_v1', __name__)
 
 api = Api(version1, catch_all_404s=True)
 
-api.add_resource(Users, '/users', strict_slashes=False)
-api.add_resource(User, '/users/<int:user_id>', strict_slashes=False)
-api.add_resource(Exercise, '/exercises', strict_slashes=False)
-api.add_resource(SingleExercise, '/exercises/<int:id>', strict_slashes=False)
-# api.add_resource(Exercise, '/workout-routines/id', strict_slashes=False)
 
-api.add_resource(WorkoutRoutine, '/workout-routines/', strict_slashes=False)
+api.add_resource(Users, '/users')
+api.add_resource(User, '/users/<int:user_id>')
+api.add_resource(Exercise, '/exercises')
+api.add_resource(SingleExercise, '/exercises/<int:id>')
+api.add_resource(SingleRoutine, '/workout-routines/<int:id>')
+
+api.add_resource(WorkoutRoutine, '/workout-routines/')
