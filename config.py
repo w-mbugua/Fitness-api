56425fa13923d53c1823e28e4cb15311f7218b94
shamuu829 <shamsodarwish@gmail.com>
2021-05-05 17:10:52 +0300

    add update

--- config.py
+++ config.py
@@ -23,6 +23,23 @@ class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://name:password@localhost/db_name'
     DEBUG = True
 
+    @app.route('/api/v1/workout/<id>', methods=['PUT'])
+def update_workout_by_id(id):
+   data = request.get_json()
+   get_workout = workout.query.get(id)
+   if data.get('title'):
+       get_workout.title = data['title']
+   if data.get('workout_description'):
+        get_workout.workout_description = data['workout_description']
+    db.session.add(get_workout)
+   db.session.commit()
+   workout_schema = workoutSchema(only=['id', 'title', 'workout_description'])
+   workout = workout_schema.dump(get_workout)
+
+   return make_response(jsonify({"workout": workout}))
+
+
+
 
 config_options = {
     'development': DevConfig,
